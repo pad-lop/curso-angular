9f05c0bfb1f313ae1f35dcc6bd3a6bcac193f8dd
unknown <luis.padilla7812@alumnos.udg.mx>
2024-06-12 20:12:49 -0600

    basic types, and interfaces

---
+++ 01-typescript-intro/src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+//import './topics/01-basic-types'
+import './topics/02-object-interface'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+
+  </div>
+`
+
---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: String = 'Strider';
+let hpPoints: Number | 'FULL' = 95;
+const isAlive: Boolean = true;
+
+hpPoints = 'FULL';
+
+console.log({
+    name, hpPoints, isAlive
+})
+
+export {};
