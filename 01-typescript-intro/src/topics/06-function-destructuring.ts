1c8584625ae0bf50affcb56016634471750a5ede
unknown <luis.padilla7812@alumnos.udg.mx>
2024-06-12 22:51:26 -0600

    06-function-destructuring completed

---
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -0,0 +1,41 @@
+interface Product {
+	description: string;
+	price: number;
+}
+
+const phone: Product = {
+	description: "Nokia",
+	price: 1500.5,
+};
+
+const tablet: Product = {
+	description: "iPad Air",
+	price: 2100.75,
+};
+
+interface TaxCalculationOptions {
+	tax: number;
+	products: Product[];
+}
+
+function taxCalculation(options: TaxCalculationOptions): [number, number] {
+	let total = 0;
+
+	let { tax, products } = options;
+
+	products.forEach(({ price }) => {
+		total += price;
+	});
+
+	return [total, total * tax];
+}
+
+const tax = 0.15;
+const shoppingCart = [phone, tablet];
+
+const [total, taxResult] = taxCalculation({
+	tax,
+	products: shoppingCart,
+});
+
+console.log(total, taxResult);
