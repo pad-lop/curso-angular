7d3901a3622e06566dcbe3058437556b650476aa
unknown <luis.padilla7812@alumnos.udg.mx>
2024-06-12 22:06:36 -0600

    04-homework-completed

--- 01-typescript-intro/src/topics/04-homework-types.ts
+++ 01-typescript-intro/src/topics/04-homework-types.ts
@@ -1,32 +1,30 @@
 interface SuperHero {
-    name: string,
-    age: number,
-    address : {
-        calle: string,
-        pais: string,
-        ciudad: string
-    }
-    showAddress: () => string
+	name: string;
+	age: number;
+	address: Address;
+	showAddress: () => string;
+}
+
+interface Address {
+	street: string;
+	country: string;
+	city: string;
 }
 
 const superHeroe: SuperHero = {
-    name: 'Spiderman',
-    age: 30,
-    address: {
-        calle: 'Main St',
-        pais: 'USA',
-        ciudad: 'NY'
-    },
-    showAddress() {
-        return this.name + ', ' + this.address.ciudad + ', ' + this.address.pais;
-    }
-}
-
+	name: "Spiderman",
+	age: 30,
+	address: {
+		street: "Main St",
+		country: "USA",
+		city: "NY",
+	},
+	showAddress() {
+		return this.name + ", " + this.address.city + ", " + this.address.country;
+	},
+};
 
 const address = superHeroe.showAddress();
-console.log( address );
-
-
-
+console.log(address);
 
 export {};
